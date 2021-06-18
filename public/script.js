29e6abee3858ecd730b81269b6a39817675d13ad
Emmanuel Najera <najeradev@gmail.com>
2021-06-18 17:28:11 -0500

    terminado

--- public/script.js
+++ public/script.js
@@ -1,74 +1,88 @@
 const container = document.querySelector(".deck");
+const containerHand = document.querySelector(".hand")
 
 fetch("/get-cards")
 	.then((data) => data.json())
 	.then((res) => onFinishedFetching(res))
 
 
-	const createCard = (card) => {
-		const number = card.slice(0, -1)
-		const symbol = card.slice(-1)
+const createCard = (card, flipped) => {
+	const number = card.slice(0, -1)
+	const symbol = card.slice(-1)
 
-		const isNumber = !isNaN(number)
-		const cardDiv = document.createElement('div')
+	const isNumber = !isNaN(number)
+	const cardDiv = document.createElement('div')
+	
+	cardDiv.classList.add('card')
+	cardDiv.setAttribute('symbol', symbol)
+	cardDiv.setAttribute('number', number)
+	
+	cardDiv.innerHTML = `
+		<div class="container">
 		
-		cardDiv.classList.add('card')
-		cardDiv.setAttribute('symbol', symbol)
-		cardDiv.setAttribute('number', number)
-		
-		cardDiv.innerHTML = `
-		 	<div class="card-corner top-left">
-		 	<div>${number}</div>
-		 	<div>${symbol}</div>
-		 	</div>
-		 	<div class="symbols">
-		 	${
-		 		number == 'A' ?
-		 		`<div class="Asymbol">${symbol}</div>` : ""
-		 	}
-		 	${
-		 		isNumber
-		 		? `${new Array(parseInt(number))
-		 			.fill(symbol)
-		 			.map(
-		 				(cardSymbol) => `
-		 				<div>${cardSymbol}</div>
-		 				`
-		 				)
-		 				.join("")}`
-		 				: ""
-		 			} 
-		 			${
-		 				number == 'J' || number == 'Q' || number == 'K' ? 
-		 				`<div class="image"></div>`
-		 				: ""
-		 			}
-		 		</div>
-		 		<div class="card-corner bottom-right">
-		 			<div>${number}</div>
-		 			<div>${symbol}</div>
-		 		</div>
-		 	`
-	cardDiv.addEventListener("click",()=>{
-		console.log("Ahora si ptm")
-	})
+		<div class="back"></div>
+		<div class="front">
+		<div class="card-corner top-left">
+		<div>${number}</div>
+		<div>${symbol}</div>
+		</div>
+		<div class="symbols">
+		${
+			number == 'A' ?
+			`<div class="Asymbol">${symbol}</div>` : ""
+		}
+		${
+			isNumber
+			? `${new Array(parseInt(number))
+				.fill(symbol)
+				.map(
+					(cardSymbol) => `
+					<div>${cardSymbol}</div>
+					`
+					)
+					.join("")}`
+					: ""
+				} 
+				${
+					number == 'J' || number == 'Q' || number == 'K' ? 
+					`<div class="image"></div>`
+					: ""
+				}
+			</div>
+			<div class="card-corner bottom-right">
+				<div>${number}</div>
+				<div>${symbol}</div>
+			</div>
+				</div>
+				</div>
+		`
 
-	return cardDiv
-	}
+if (flipped) {
+	cardDiv.classList.add("flipped")	
+}
+cardDiv.addEventListener("click",()=>{	
+	cardDiv.classList.contains("flipped")?
+	cardDiv.classList.remove("flipped") :
+	cardDiv.classList.add("flipped")
+})
+
+return cardDiv
+}
 
 function onFinishedFetching(res) {
 	const { deck, hands } = res;
-	
-	deck.forEach((card) => {
-        container.append(createCard(card))
-	})
-	container.innerHTML += `<h2>Your Hand</h2>`;
 
-	 hands.forEach((hand) => {
-	 	const newHand = hand.map(createCard);
-		newHand.forEach((card)=>{
-			container.appendChild(card)
+	deck.forEach((card, index)=> {
+		container.appendChild(createCard(card,(index < 2)))
+		
+	});
+	
+	hands.forEach((hand)=>{
+		hand.forEach((card,index)=>{
+			containerHand.appendChild(createCard(card,(index < 2)))
 		})
-	 });
+	})
+
+
 }
 
