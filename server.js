bf1cd2da5748d27235db7bc12368da5b26ed15ed
Emmanuel Najera <najeradev@gmail.com>
2021-06-22 00:33:06 -0500

    server routes are now fixxed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const { Deck, Hand } = require('./cards/deck.js');
+app.use(cors());
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -8,15 +10,19 @@ app.use(express.json());
 const deck = new Deck()
 const table = new Hand(deck, +5)
 
-app.get('/get-cards', (req, res) => {
-  const hands = [];
-  hands.push(new Hand(deck, +2))
+app.get('/hand', (req, res) => {
+  const hands = new Hand(deck, +2)
   return res.json({
-    hands: hands.map((hand) => hand.cards),
-    deck: table.cards
+    hand: hands.cards
   })
 })
   
+app.get('/table', (req, res) => {
+  return res.json({
+    deck: table.cards
+  })
+})
+
 app.listen(4001, () => {
   console.log('Server running on port 4001');
 });
